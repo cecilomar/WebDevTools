90571ff0bcd9f7ea0f56eeed44c508732655d2ce
Cecil O. Almonte <info@cecilomar.com>
2013-08-22 16:38:12 -0400

    Initial File Structure

---
+++ mod_webdevtools/mod_webdevtools.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @package    WebDevTools
+* @author     Cecil O. Almonte
+* @copyright  Copyright (C) Cecilomar Design, Inc. 2013
+* @license    http://www.gnu.org/licenses/gpl.txt
+**/
+
+//no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+require_once (dirname ( __FILE__ ) . "/helper.php");
+
+require(JModuleHelper::getLayoutPath( 'mod_webdevtools' ));
+
+?>
